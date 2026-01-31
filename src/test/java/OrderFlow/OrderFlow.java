b0da0792865d771b459be5917ccb39eceaa2e32a
Kishore Vatti <kishorevatti73@gmail.com>
2026-01-31 13:38:38 +0530

    Expected to Fail

--- src/test/java/OrderFlow/OrderFlow.java
+++ src/test/java/OrderFlow/OrderFlow.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -123,7 +124,8 @@ public class OrderFlow {
 		
 		
 		
-		
+		Assert.fail("Fail test intentionally");
+
 		
 		
 		
