1ac15d9bc07d65b643a8befa4a2d02ed6d9f3f98
Kishore Vatti <kishorevatti73@gmail.com>
2026-01-31 23:26:43 +0530

    chrome options set

--- src/test/java/OrderFlow/BaseTest.java
+++ src/test/java/OrderFlow/BaseTest.java
@@ -1,11 +1,14 @@
 package OrderFlow;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
 import Helpers.Waits;
 
 public class BaseTest {
@@ -15,7 +18,16 @@ public class BaseTest {
 
     @BeforeClass
     public void browserSetup() {
-        driver = new ChromeDriver();
+    	ChromeOptions options = new ChromeOptions();
+
+    	options.addArguments("--headless=new");
+    	options.addArguments("--no-sandbox");
+    	options.addArguments("--disable-dev-shm-usage");
+    	options.addArguments("--disable-gpu");
+    	options.addArguments("--window-size=1920,1080");
+
+    	driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         wait = new WebDriverWait(driver, Duration.ofSeconds(Waits.ImplicitWait));
